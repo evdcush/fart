9661cb0a839a68e72f376a1b6dfeef7eb7d12593
Evan Cushing <evdcush@protonmail.com>
2024-01-03 10:35:06 +0900

    Correct package name in `__init__.py` files

--- fart/__init__.py
+++ fart/__init__.py
@@ -1,12 +1,11 @@
-""" pyfart - Figlet ART for documentation """
+"""fart - Figlet ART for documentation"""
 import os
 import sys
 
-__version__ = '0.1.4'
+__version__ = '0.2.0'
 __author__ = 'Evan Cushing <evdcush@protonmail.com>'
-#__all__ = []
 
-#=== Pathing
+#=== Pathing hack
 project_path = os.path.abspath(os.path.dirname(__file__))
 if project_path not in sys.path:
     sys.path.append(project_path)
